9753b07a9c1867c65d348714ed229edd0ba17939
pengshi2 <boogiesnoogawoot@gmail.com>
2023-04-17 10:05:15 -0400

    n

--- nyc-api-project/src/components/HelloWorld.vue
+++ nyc-api-project/src/components/HelloWorld.vue
@@ -11,7 +11,8 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      hey guys
+
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
