b12ba3c9f35972bf802903bf4abd9b582367717b
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-04-17 10:17:07 -0400

    f

--- nyc-api-project/src/components/HelloWorld.vue
+++ nyc-api-project/src/components/HelloWorld.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+async function getAmiibos() {
+  try {
+    const response = await fetch("https://data.cityofnewyork.us/resource/f9bf-2cp4.json");
+    const amiibosData = await response.json();
+    if (response.status < 200 || response.status > 299) {
+      console.log("poop");
+    }
+    const amiibos = amiibosData.amiibo;
+  } catch (error) {
+    console.log(error);
+    console.log("big poo");
+  }
+</script>
 
 <template></template>
 
-<style scoped></style>
+<style></style>
