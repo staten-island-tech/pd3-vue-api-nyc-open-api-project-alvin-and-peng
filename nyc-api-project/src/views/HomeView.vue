8b09cea8c3f0ea1c9a14fcc4f275ffa2abdbbb5e
pengshi2 <boogiesnoogawoot@gmail.com>
2023-04-19 10:14:37 -0400

    zx

--- nyc-api-project/src/views/HomeView.vue
+++ nyc-api-project/src/views/HomeView.vue
@@ -2,9 +2,9 @@
 import { ref, onMounted } from 'vue'
 const water = ref('')
 async function getWater() {
-  let response = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json')
-  let data = await response.json()
-  water.value = data.results
+  const response = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json')
+  const data = await response.json()
+  water.value = data.total
 }
 
 onMounted(() => {
