6a28aa21df9ee415dc6c42ce4201039238f1c785
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-04-19 10:15:35 -0400

    v

--- nyc-api-project/src/views/HomeView.vue
+++ nyc-api-project/src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-const water = ref('')
+const year = ref('')
 async function getWater() {
   let res = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json')
   let data = await res.json()
-  water.value = data.results
+  year.value = data.results
 }
 onMounted(() => {
   getWater()
