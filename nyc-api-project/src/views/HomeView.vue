03fe405dc921ba262b8ed5e241b7bc59b2d4f168
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-04-19 09:53:18 -0400

    sc

--- nyc-api-project/src/views/HomeView.vue
+++ nyc-api-project/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const water = ref('')
+async function getWater() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json')
+  let data = await res.json()
+  water.value = data.results
+}
+onMounted(() => {
+  getWater()
+})
 </script>
 
 <template>
