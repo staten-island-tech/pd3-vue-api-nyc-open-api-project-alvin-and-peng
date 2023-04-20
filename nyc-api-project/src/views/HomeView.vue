55ee0ad58795d124fc43fdd3b2517a618b3bda35
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-04-20 10:16:54 -0400

    asdf

--- nyc-api-project/src/views/HomeView.vue
+++ nyc-api-project/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import waterDatacard from '../components/waterDatacard.vue'
 const water = ref('')
 async function getWater() {
   let res = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json')
@@ -12,5 +13,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <main></main>
+  <div class="container">
+    <waterDatacard
+      v-for="(title, index) in water"
+      :key="title.year"
+      :id="index + 1"
+      :water="title"
+    />
+  </div>
 </template>
