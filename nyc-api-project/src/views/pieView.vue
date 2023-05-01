ee3f2802103c718abf592b2396b7398dd9ea9632
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-05-01 19:22:16 -0400

    k

--- nyc-api-project/src/views/pieView.vue
+++ nyc-api-project/src/views/pieView.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <main>
     <NycConsumptionChart />
   </main>
@@ -7,4 +7,33 @@
 <script>
 import NycConsumptionChart from '../components/pieChart.vue'
 
-</script>
+</script> -->
+
+<template>
+  <div class="contain">
+    Years for Average Consumption of Water Per Person (Gallons)
+    <BarChart v-if="loaded" :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script>
+import BarChart from '../components/BarChart.vue'
+import {} from 'vue'
+export default {
+  name: 'BarView',
+  components: { BarChart },
+  data() {
+    return {
+      loaded: false,
+      chartData: {
+        labels: ['110-120', '130-140', '150-160', '170-180', '190-200', '200+'],
+        datasets: [{ data: [] }]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true,
+        backgroundColor: ['#caf0f8', '#ADD8f9', '#90e0ef', '#00b4d8', '#0077b6']
+      }
+    }
+  },
+
