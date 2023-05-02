ee068ae474f6542573dd97b4f8fb3b7f0042d4ad
pengshi2 <boogiesnoogawoot@gmail.com>
2023-05-02 10:00:44 -0400

    aaa

--- nyc-api-project/src/router/index.js
+++ nyc-api-project/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import apiData from '../views/apiData.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'ApiData',
-      component: () => import('/views/apiData.vue')
+      name: 'apiData',
+      component: apiData
     },
+    {
+      path: '/apiData',
+      name: 'ApiData',
+      component: () => import('../views/apiData.vue')
+    },
+
     {
       path: '/pieView',
       name: 'pieView',
