825250ace222eebbdc97b26060663da7cca71ee5
pengshi2 <pengshi0921@gmail.com>
2023-05-01 20:47:44 -0400

    C

--- nyc-api-project/src/router/index.js
+++ nyc-api-project/src/router/index.js
@@ -10,6 +10,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/pieView.vue')
+    },
+
+    {
+      path: '/homeView',
+      name: 'HomeView',
+      component: () => import('../views/HomeView.vue')
     }
   ]
 })
